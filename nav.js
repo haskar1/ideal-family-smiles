8b8390d671b794019ef5af9d3a700b8cf98e6f17
Hussein <gimble20@yahoo.com>
2022-11-18 14:20:21 -0500

    Improve navigation functionality

--- nav.js
+++ nav.js
@@ -3,8 +3,31 @@
 //
 const navbarMenu = document.querySelector("#navigation #navbar-menu");
 const hamburgerMenu = document.querySelector("#navigation .hamburger-menu");
+const mainSection = document.querySelector('main');
+const footer = document.querySelector('footer');
 
-hamburgerMenu.addEventListener('click', function() {
+hamburgerMenu.addEventListener('click', () => {
     navbarMenu.classList.toggle("open");
     hamburgerMenu.classList.toggle("clicked");
+    
+    closeNavbarOnOutsideClick();  //close navbar when you click outside of it
 });
+
+function closeNavbarOnOutsideClick() {
+    if (navbarMenu.classList.contains("open")) {
+        mainSection.addEventListener('click', removeMenuClasses)
+        footer.addEventListener('click', removeMenuClasses)
+    }
+    else {
+        mainSection.removeEventListener('click', removeMenuClasses)
+        footer.removeEventListener('click', removeMenuClasses)
+    }
+}
+
+function removeMenuClasses() {
+    navbarMenu.classList.remove("open");
+    hamburgerMenu.classList.remove("clicked");
+
+    mainSection.removeEventListener('click', removeMenuClasses);
+    footer.removeEventListener('click', removeMenuClasses)
+}
