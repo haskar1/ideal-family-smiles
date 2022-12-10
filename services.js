820b9d940de6af067637e14b8d78a9a8c9108833
Hussein <gimble20@yahoo.com>
2022-12-09 19:14:53 -0500

    Refactor services page code

---
+++ services.js
@@ -0,0 +1,88 @@
+/******************************
+ Adds 'clicked' attribute to services when clicked to allow description to expand. 
+ Also collapses any other open service descriptions.
+ ******************************/
+
+const services = document.querySelectorAll('#services ul li span');
+
+[...services].forEach(service => 
+  service.addEventListener('click', (e) => {
+    let clickedService = e.target;
+
+    clickedService.toggleAttribute('clicked');
+    collapseOtherServices(clickedService);
+  })
+)
+
+function collapseOtherServices(clickedService) {
+  [...services].forEach(service => {
+    if (service !== clickedService) {
+      service.removeAttribute('clicked');
+    }
+  })
+}
+
+
+
+/******************************
+ Collapse all services when you click outside of the services section (if you click on whitespace on the page)
+ ******************************/
+
+ document.addEventListener('click', (e) => {
+  if (e.target.matches('#services ul li *')) {
+    return;
+  }
+
+  collapseAllServices();
+})
+
+function collapseAllServices() {
+  [...services].forEach(service => {
+    service.removeAttribute('clicked');
+  })
+}
+
+
+
+/******************************
+ Fixes a bug when you click on service in viewport < 900px, then change viewport to >= 900px...
+ The bug: after clicking a service in small viewport, the description (.slide) would stay open even after changing viewport, 
+ but the service's name (span) would also be visible, so they'd overlap.
+ ******************************/
+
+// Create a condition that targets viewports at least 900px wide
+const mediaQuery = window.matchMedia('(min-width: 900px)');
+let viewportPassedMinWidth = false;
+
+function handleViewportChange(e) {
+  // Check if the media query is true
+  if (e.matches) {
+    collapseServiceOnMouseleave(e);
+    viewportPassedMinWidth = true;
+  }
+  
+  else if (!e.matches && viewportPassedMinWidth === true) {
+    [...services].forEach(service => 
+      service.removeEventListener('mouseleave', removeClickedAttribute)
+    )
+
+    viewportPassedMinWidth = false;
+  }
+}
+
+function collapseServiceOnMouseleave(e) {
+  [...services].forEach(service => 
+    service.addEventListener('mouseleave', removeClickedAttribute)
+  )
+}
+
+function removeClickedAttribute(e) {
+  let clickedService = e.currentTarget;
+  clickedService.removeAttribute('clicked');
+}
+
+// Register event listener
+mediaQuery.addListener(handleViewportChange)
+
+// Initial check
+handleViewportChange(mediaQuery)
